2b1a187be6613951405a26321fbf38acd1108d44
MichaelKapranos <Mick.kapranos@gmail.com>
2024-12-30 23:44:12 +0000

    new pages

---
+++ src/Accomplishments.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Accomplishments: React.FC = () => {
+  return (
+    <div>
+      <h1>Accomplishments & Certifications</h1>
+      <p>This is the accomplishments page.</p>
+    </div>
+  );
+};
+
+export default Accomplishments;
