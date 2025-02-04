209765a119c20d51a5c00911517509930d6be25b
MichaelKapranos <Mick.kapranos@gmail.com>
2025-02-04 17:07:14 +0000

    ^

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaHome, FaChartBar, FaProjectDiagram, FaTrophy } from 'react-icons/fa';
 import './Sidebar.css';
