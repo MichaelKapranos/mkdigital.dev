65056862238d7b9c9a4b2301d6b784dea939cff9
MichaelKapranos <Mick.kapranos@gmail.com>
2025-02-04 17:38:51 +0000

    temp broken

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import AboutMe from './AboutMe';
-import Accomplishments from './Accomplishments';
-import Statistics from './Statistics';
-import Projects from './Projects';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Sidebar from './Sidebar';
+// import AboutMe from './AboutMe';
+// import Accomplishments from './Accomplishments';
+// import Statistics from './Statistics';
+// import Projects from './Projects';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<AboutMe />} />
-        <Route path="/accomplishments" element={<Accomplishments />} />
-        <Route path="/statistics" element={<Statistics />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Sidebar />
+    //   <Routes>
+    //     <Route path="/" element={<AboutMe />} />
+    //     <Route path="/accomplishments" element={<Accomplishments />} />
+    //     <Route path="/statistics" element={<Statistics />} />
+    //     <Route path="/projects" element={<Projects />} />
+    //   </Routes>
+    // </Router>
+    <div></div>
   );
 };
 
